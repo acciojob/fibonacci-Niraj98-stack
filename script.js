33635ace6643ebf105c620e7f6d3d47d18e2c5a8
Niraj chaudhari <151720495+Niraj98-stack@users.noreply.github.com>
2024-03-11 20:44:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    
+    let prev = 0;
+    let current = 1;
+    let next;
+    
+    for (let i = 2; i <= num; i++) {
+        next = prev + current;
+        prev = current;
+        current = next;
+    }
+    
+    return prev;
 }
 
-module.exports = fibonacci;
+// Test cases
+console.log(fibonacci(1)); // 
+console.log(fibonacci(5)); 
